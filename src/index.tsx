62409d5672d67bfa1016832bf3a1fe80cabe892d
Chingis Choybsonov <avatarius93@gmail.com>
2024-05-10 21:40:02 +0300

    открывание меню

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
+import { ArrowButton } from './components/arrow-button';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,6 +14,19 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [menuShown, setMenuShown] = useState(false);
+	/* const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth
+	); */
+
 	return (
 		<div
 			className={clsx(styles.main)}
@@ -25,7 +39,15 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				menuShown={menuShown}
+				arrowButton={
+					<ArrowButton
+						menuShown={menuShown}
+						onClick={() => setMenuShown((prev) => !prev)}
+					/>
+				}
+			/>
 			<Article />
 		</div>
 	);
